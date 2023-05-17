c34944231799eb1318b31d9aef4f741adbe14adb
hattori ryuki <ryuki.97913795@i.softbank.jp>
2023-05-18 01:05:25 +0900

    Add: canvasで4項目のルーレットを描画

--- src/index.js
+++ src/index.js
@@ -1,2 +1,43 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
+let radius = 220;
+let angle = 0;
+let deg_count = 90;
+let data = [
+  {
+    name: "赤色",
+    color: "#FF597F"
+  },
+  {
+    name: "緑色",
+    color: "#8FFFBE"
+  },
+  {
+    name: "青色",
+    color: "#8EBEFF"
+  },
+  {
+    name: "黄色",
+    color: "#FDED75"
+  }
+];
+
+ctx.translate(canvas.width / 2, canvas.height / 2);
+
+function drawRoullet(offset) {
+  let sum_deg = 0;
+  
+  data.forEach((e) => {
+    angle = sum_deg + offset;
+    let start_deg = ((360 - angle) * Math.PI) / 180;
+    let end_deg = ((360 - (angle + deg_count)) * Math.PI) / 180;
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.fillStyle = e.color;
+    ctx.arc(0, 0, radius, start_deg, end_deg, true);
+    ctx.fill();
+    sum_deg += deg_count;
+  });
+}
+drawRoullet(0);
+
